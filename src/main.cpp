15ffb906e8105823564d528a7f5fb767b92edb18
Maximilian Deubel <maximilian.deubel@ovgu.de>
2019-10-25 02:42:58 +0200

    macOS compat

--- src/main.cpp
+++ src/main.cpp
@@ -119,6 +119,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     // Core Profile - no backwards compatibility
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Hooray!", NULL, NULL);
     if (!mainWindow) {
